bd29f71a2356c6028d0c3d8d2b6f45809611a776
Tyler Dougherty <tylerdougherty@me.com>
2016-02-22 21:31:37 -0500

    The search really does stuff + code style fixes

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -9,13 +9,13 @@ class ArchiveController < ApplicationController
             # response = Net::HTTP.get(uri)
             # @json = JSON.parse(response)
 
-            url = 'https://archive.org/advancedsearch.php?q="title:"moby dick" mediatype:"texts""&fl[]=downloads,format,identifier,title&output="json"&sort[]=downloads desc&rows=50&page=1'
+            url = "https://archive.org/advancedsearch.php?q=\"title:\"#{params[:search]}\" mediatype:\"texts\"\"&fl[]=downloads,format,identifier,title&output=\"json\"&sort[]=downloads desc&rows=50&page=1"
             uri = URI(url)
             response = Net::HTTP.get(uri)
             @json = JSON.parse(response)
 
             @results = @json['response']['numFound']
-            @docs = @json['response']['docs']
+            @docs = @json['response']['docs'].select { |doc| doc['format'].map{|it| it.downcase.include? 'abbyy'}.include? true } # only take results with Abbyy results
         else
             # nothing right now
         end
