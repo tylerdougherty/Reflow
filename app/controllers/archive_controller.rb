115ea687b551cb586ab485e98d81055e9f66cfa0
Tyler Dougherty <tylerdougherty@me.com>
2016-02-29 19:12:30 -0500

    Added a pager and the page number parameter to the web call for archive.

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -4,12 +4,15 @@ require 'json'
 class ArchiveController < ApplicationController
     def example
         if params[:search]
+            @page = (defined? params[:page]) ? params[:page] : 1
+
+            # Showing the details/metadata of a particular item
             # url = params[:search]
             # uri = URI(url)
             # response = Net::HTTP.get(uri)
             # @json = JSON.parse(response)
 
-            url = "https://archive.org/advancedsearch.php?q=\"title:\"#{params[:search]}\" mediatype:\"texts\"\"&fl[]=downloads,format,identifier,title&output=\"json\"&sort[]=downloads desc&rows=50&page=1"
+            url = "https://archive.org/advancedsearch.php?q=\"title:\"#{params[:search]}\" mediatype:\"texts\"\"&fl[]=downloads,format,identifier,title&output=\"json\"&sort[]=downloads desc&rows=50&page=#{@page}"
             uri = URI(url)
             response = Net::HTTP.get(uri)
             @json = JSON.parse(response)
