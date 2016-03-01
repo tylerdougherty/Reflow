d12e9068af10372c231333b1425ab6c5618d0465
Tyler Dougherty <tylerdougherty@me.com>
2016-02-29 20:18:00 -0500

    Next/Prev buttons work now

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 class ArchiveController < ApplicationController
     def example
         if params[:search]
-            @page = (defined? params[:page]) ? params[:page] : 1
+            @page = (params[:page] != nil) ? params[:page].to_i : 1
 
             # Showing the details/metadata of a particular item
             # url = params[:search]
@@ -19,6 +19,8 @@ class ArchiveController < ApplicationController
 
             @results = @json['response']['numFound']
             @docs = @json['response']['docs'].select { |doc| doc['format'].map{|it| it.downcase.include? 'abbyy'}.include? true } # only take results with Abbyy results
+            @has_next = @results > @page*50
+            @has_prev = @page > 1
         else
             # nothing right now
         end
