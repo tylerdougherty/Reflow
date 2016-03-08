8e5c7ddc2f07a6411f20b680f47d9c950544300e
Tyler Dougherty <tylerdougherty@me.com>
2016-03-08 00:34:32 -0500

    Made the reflow controller paging code a bit easier to read

--- app/controllers/reflow_controller.rb
+++ app/controllers/reflow_controller.rb
@@ -1,14 +1,13 @@
 class ReflowController < ApplicationController
-  def index
-      @books = Book.all
-  end
-  def text
-      @books = Book.all
-      @pages = Page.joins(:book).where('books.id' => request.original_url.split('/').last )
-      page = "page=#{@currentPage}"
-      @pageNum = (params[:page] != nil) ? params[:page].to_i : 1
-      @hasNext = @pages.length > @pageNum
-      @hasPrevious = @pageNum > 1
-      @currentPage = @pages.find_by(number: @pageNum)
-  end
+    def index
+        @books = Book.all
+    end
+
+    def text
+        @pages = Page.where(book_id: params[:id])
+        @pageNum = (params[:page] != nil) ? params[:page].to_i : 1
+        @hasNext = @pages.length > @pageNum
+        @hasPrevious = @pageNum > 1
+        @currentPage = @pages.find_by(number: @pageNum)
+    end
 end
