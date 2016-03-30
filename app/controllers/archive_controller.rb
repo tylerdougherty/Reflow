755580d6af04b31c383c8be0280926d8c63683b1
Tyler Dougherty <tylerdougherty@me.com>
2016-03-29 22:15:21 -0400

    Fixed archive search to actually filter based on available file types and page correctly.

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -13,18 +13,18 @@ class ArchiveController < ApplicationController
             page_size = 50
 
             search_url = 'https://archive.org/advancedsearch.php'
-            # TODO: escape search query
-            query = "q=\"title:\"#{params[:search]}\" mediatype:\"texts\"\""
+            # TODO: escape search query (to prevent malicious queries)
+            query = "q=\"title:\"#{params[:search]}\" mediatype:\"texts\"\" format:\"Abbyy GZ\" AND format:\"Single Page Processed JP2 ZIP\""
             requested_info = 'fl[]=downloads,format,identifier,title'
             output = 'output=\"json\"'
             sort = 'sort[]=downloads desc'
             rows = "rows=#{page_size}"
             page = "page=#{@page}"
+
             @json = get_json_response "#{search_url}?#{query}&#{requested_info}&#{output}&#{sort}&#{rows}&#{page}"
 
-            # TODO: FIX THIS SEARCH WHEN IT WORKS ON ARCHIVE.ORG AGAIN
-            @docs = @json['response']['docs'].select { |doc| doc['format'].map{|it| it.downcase.include? 'abbyy'}.include? true }
-            @results = @docs.count # TODO: <---------
+            @docs = @json['response']['docs']
+            @results = @json['response']['numFound']
             @has_next = @results > @page*page_size
             @has_prev = @page > 1
         else
@@ -40,6 +40,7 @@ class ArchiveController < ApplicationController
         f1 = @files.each.select{|x| x['format'] == 'Abbyy GZ'}[0]['name']
         f2 = @files.each.select{|x| x['format'] == 'Single Page Processed JP2 ZIP'}[0]['name']
 
+        # May be unnecessary now but it doesn't hurt to make sure
         if f1.nil? or f2.nil?
             success = false
             message = 'Download failed: Needed files do not exist!'
